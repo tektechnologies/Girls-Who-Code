992c21dba3fa959564eec77340f5304854023b56
tektechnologies <info@tektechnologies.com>
2024-02-28 20:13:06 -0600

    add starter code for while for loops

---
+++ chat-bot-starterCode.py
@@ -0,0 +1,38 @@
+#Meet my Chatbot Friend Starter Code
+
+
+print("Hello! I'm NAME the chatbot.")
+name = input("What's your name? ")
+
+print("Let's see if I can spell that:")
+
+#Add Part 5 code here
+
+
+print("Nice to meet you " + name)
+
+
+print("Can you tell me about yourself?")
+
+#add Part 2 Step 2 code in this section
+print("Where are you from?")
+place = input("You are from... ")
+
+
+print("What are you passionate about?")
+passion = input("You are passionate about... ")
+
+
+print("What is your personal goal?")
+goal = input("You hope to... ")
+
+
+
+#add Part 4 code here
+
+topic = input("What would you like to know about me? - home, passion, goal, or none? ")
+
+#add Part 3 code here
+
+
+print("Thanks for chatting with me! See you soon!")
